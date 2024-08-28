a141bc67aa38761be89ea10d1cb7e404f52b6a22
AjithJoshy <117666438+joshyajith863@users.noreply.github.com>
2024-08-28 11:29:18 +0530

    Add files via upload

---
+++ Inheritance in Java/inheritance_Employeee.java
@@ -0,0 +1,11 @@
+
+public class inheritance_Employeee {
+    public void display(){
+        System.out.println("The class is inheritance_Employee");
+    }
+   public  int calcSalary(int salary ){
+        System.out.println("Salary is "+salary);
+        return 0;
+    }
+   
+}
