2e3a81f9cfe9220ec87ac700e850363bf0508124
AjithJoshy <117666438+joshyajith863@users.noreply.github.com>
2024-09-30 14:58:09 +0530

    Update Dog.java

--- Interface_Inheritance/Dog.java
+++ Interface_Inheritance/Dog.java
@@ -5,33 +5,43 @@
     */
     //Importing the scanner class to get input 
  import java.util.Scanner;
- public class Dog extends Animal{
-    public Dog(int legs){
-        super(legs);
-    }
-    public void setName(String Name){
-        this.Name = Name;
-    }
-    public void getName(){
-        if(name != null){
-            return name;
-        }
-        else{
-            System.out.println("Please provide a name ");
-        }
-    }
-    public void play(){
-        System.out.println(Name +"is playing");
-    }
-    public static void main(String [] args){
-        Dog dog = new Dog();
-        //Created an instance for the object dog
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the name of the dog");
-        String name = sc.nextLine();
-        dog.setName(name);
-        System.out.println(dog.getName() + "is your petdog....");
-        dog.play();
+
+// Dog class extends Animal and implements the Pet interface
+public class Dog extends Animal implements Pet {
+    private String Name;
+
+    public Dog(int legs) {
+        super(legs);  // Calls Animal constructor
     }
 
- }
+    // Implementation of setName method from Pet interface
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    // Implementation of getName method from Pet interface
+    public String getName() {
+        if (this.Name != null) {
+            return this.Name;
+        } else {
+            return "Please provide a name";
+        }
+    }
+
+    // Implementation of play method from Pet interface
+    public void play() {
+        System.out.println(Name + " is playing");
+    }
+
+    public static void main(String[] args) {
+        Dog dog = new Dog(4);  // Creates Dog object with 4 legs
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the name of the dog:");
+        String name = sc.nextLine();
+        dog.setName(name);
+
+        System.out.println(dog.getName() + " is your pet dog...");
+        dog.play();
+    }
+}
